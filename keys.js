1819de01fe0fa50b305e12e6c1e8159d44d3e2a9
Jalil <ja@Jas-MacBook-Pro.local>
2018-04-12 16:38:58 -0400

    tweets work but spotify has rules preventing me from testing

--- keys.js
+++ keys.js
@@ -1,13 +1,8 @@
-console.log('this is loaded');
+console.log("This is running");
 
-exports.twitter = {
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-};
-
-exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
-};
+exports.twitterKeys = {
+    consumer_key: 'Ts06BL5bpPWQhYOeSy9u2QFgN',
+    consumer_secret: '88pdJh8LCnIRatblZe7FBViuHqQD436GrLZS8YJkwtxyjuFN2r',
+    access_token_key: '470488150-xFhk5mQUqW4hLNUtUlhiUgf6A1GZaqsAvuNeTZjD',
+    access_token_secret: 'ly21yaW6kGS8doiyG4rpEU1eveGx9E9gVqkO8Bph4wvSK',
+}
--- liri.js
+++ liri.js
@@ -1,44 +1,74 @@
-var request = require("request");
-require("dotenv").config();
-
-  var spotify = new Spotify(keys.spotify);
-  var client = new Twitter(keys.twitter);
-
-  var apiKey = 'Ts06BL5bpPWQhYOeSy9u2QFgN';
-var options = {
-  provider: 'twitter',
-  
-  httpAdapter: 'https', // Default
-  apiKey: apiKey,
-};
-var Spotify = require('node-spotify-api');
+var keys = require ('./keys.js');
+var Twitter = require('twitter');
+var spotify = require('spotify');
+ var getMyTweets = function(){
+    var client = new Twitter(keys.twitterKeys);
  
-var spotify = new Spotify({
-  id: <b90f602a84d9410a9593ed5f83e2a1a6>,
-  secret: <3973a8213d7b45abb9d9750cdaec9afa>
+var params = {screen_name: 'jalil_L'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+    for (i=0;i<tweets.length;i++){
+        console.log(tweets[i].created_at);
+        console.log(' ');
+        console.log(tweets[i].text);
+    }
+  }
 });
+}
+ var getMeSpotify = function(songName) {
+spotify.search({ type: 'track', query: 'songName' }, 
+    function(err, data) {
+     if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
  
-spotify
-  .search({ type: 'track', query: 'All the Small Things' })
-  .then(function(response) {
-    console.log(response);
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
-  // run a request to spotify
+    console.log(data.tracks.items[0]);
+});
+ }
+var pick = function(caseData, functionData){
+    switch(caseData) {
+        case 'my-tweets' :
+        getMyTweets();
+        break;
+        case 'spotify-this-song':
+        getMeSpotify(functionData);
+        break;
+        case "movie-this":
+      getMeMovie(functionData);
+      break;
 
-  song = encodeURI(process.argv[2])
-
-  var query = song;
-
-  console.log(query);
-
-  request(query, function(error, response, body){
-  	if (!error && response.statusCode === 200){
-  		console.log(JSON.parse(body).Year);
-  	}
-  })
-  // store variables here
-
+        default:
+        
+        console.log('LIRI dose not know that')
+    }
+}
+var getMeMovie = function(movieName) {
+    if (movieName === undefined) {
+      movieName = "Mr Nobody";
+    }
   
+    var urlHit = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy";
+  
+    request(urlHit, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        var jsonData = JSON.parse(body);
+  
+        console.log("Title: " + jsonData.Title);
+        console.log("Year: " + jsonData.Year);
+        console.log("Rated: " + jsonData.Rated);
+        console.log("IMDB Rating: " + jsonData.imdbRating);
+        console.log("Country: " + jsonData.Country);
+        console.log("Language: " + jsonData.Language);
+        console.log("Plot: " + jsonData.Plot);
+        console.log("Actors: " + jsonData.Actors);
+        console.log("Rotton Tomatoes Rating: " + jsonData.Ratings[1].Value);
+      }
+    });
+  };
+  
+var runthis = function(argOne, argTwo){
+    pick(argOne, argTwo);
+};
+runthis(process.argv[2], process.argv[3]);
